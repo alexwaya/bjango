60e95d3983233c21f29ebb5b00af79d295624788
Alexander Waya Nguku <alex.waya@gmail.com>
2022-01-29 18:12:49 +0300

    landing page extends base

--- doctor/views.py
+++ doctor/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 # Create your views here.
-def home(request):
-    return HttpResponse("It's working")
+# def home(request):
+#     return HttpResponse("It's working")
+
+
+class HomeTemplateView(TemplateView):
+    template_name = "index.html"
