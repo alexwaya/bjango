c7e1d68136a00ae693973c70430d46947ae5ebd9
Alexander Waya Nguku <alex.waya@gmail.com>
2022-01-30 23:11:02 +0300

    Enabled sending of emails

--- doctor/views.py
+++ doctor/views.py
@@ -1,11 +1,27 @@
+from email import message
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
+from django.core.mail import EmailMessage, message
 
-# Create your views here.
-# def home(request):
-#     return HttpResponse("It's working")
+from django.conf import settings
 
 
 class HomeTemplateView(TemplateView):
     template_name = "index.html"
+
+    def post(self, request):
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
+
+        email = EmailMessage(
+            subject= f"{name} from doctor family.",
+            body=message,
+            from_email= settings.EMAIL_HOST_USER,
+            to=[settings.EMAIL_HOST_USER],
+            reply_to=[email]
+        )
+
+        email.send()
+        return HttpResponse("Email sent successfully")
